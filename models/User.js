5727fd228f808ba09a69b4a1f8b04890abd36c42
Avani <avani.jadeja@gmail.com>
2023-06-09 20:42:16 -0500

    Add User model in models folder

---
+++ models/User.js
@@ -0,0 +1,41 @@
+// require mongoose for model and Schema
+const { Schema, model } = require("mongoose");
+
+// Schema to create a User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
