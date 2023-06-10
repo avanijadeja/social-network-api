23804a8c72446bd972e4410d75d350abba44010a
Avani <avani.jadeja@gmail.com>
2023-06-10 10:21:13 -0500

    Created creteUser function in user-controller.js file

---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+// require models User and Thought
+const { User, Thought } = require("../models");
+
+const userController = {
+  // Create a new User
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
