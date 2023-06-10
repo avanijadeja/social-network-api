0e0ce78080e06daac19a25466cd00efe65f39b12
Avani <avani.jadeja@gmail.com>
2023-06-10 10:59:01 -0500

    Added logic for create thought in thought-controller.js file

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({
+            message: "Thought created, but found no user with that ID",
+          });
+        }
+        res.json({ message: "Thought Created 🎉" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
