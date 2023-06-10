d318272c57e71b8202620f2e5ca1fc805cfa8bbc
Avani <avani.jadeja@gmail.com>
2023-06-10 18:55:21 -0500

    Added updatethought in thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,12 +5,13 @@ const {
   createThought,
   getThoughts,
   getSingleThought,
+  updateThought,
 } = require("../../controllers/thought-controller");
 
 // set route /api/thoughts for function createThought
 router.route("/").post(createThought).get(getThoughts);
 
 // set route /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought);
+router.route("/:thoughtId").get(getSingleThought).put(updateThought);
 // export router
 module.exports = router;
