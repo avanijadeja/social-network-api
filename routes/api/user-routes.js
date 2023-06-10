aee6aac12742ee455d2c12f8e29ef1f4a5ce113c
Avani <avani.jadeja@gmail.com>
2023-06-10 17:59:08 -0500

    Delete user in user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,13 +6,14 @@ const {
   getUsers,
   getSingleUser,
   updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
 //  for route /api/users
 router.route("/").post(createUser).get(getUsers);
 
 //  for route /api/users/:userId
-router.route("/:userId").get(getSingleUser).put(updateUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // export router
 module.exports = router;
