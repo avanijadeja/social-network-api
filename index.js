f870bf348a7e7e9a00d6ac7aed8cd20b4e99b394
Avani <avani.jadeja@gmail.com>
2023-06-09 20:13:57 -0500

    Created index.js main file

---
+++ index.js
@@ -0,0 +1,26 @@
+// Dependencies
+//require express
+const express = require('express');
+// require connection.js
+const db = require('./config/connection');
+// require routes
+const routes = require('./routes');
+
+
+const cwd = process.cwd();
+
+// setup express app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// setup the routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// start the server
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
