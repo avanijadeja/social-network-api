6ad9d1b7059b2da9e8510b1bce4d0ac499208e1e
Avani <avani.jadeja@gmail.com>
2023-06-10 17:02:42 -0500

    Added singleUser route in user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,15 @@
 // require express router
 const router = require("express").Router();
 
-const { createUser, getUsers } = require("../../controllers/user-controller");
+const {
+  createUser,
+  getUsers,
+  getSingleUser,
+} = require("../../controllers/user-controller");
 
 //  for route /api/users
 router.route("/").post(createUser).get(getUsers);
 
+//  for route /api/users/:userId
+router.route("/:userId").get(getSingleUser);
 module.exports = router;
