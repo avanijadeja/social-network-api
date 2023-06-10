961bed82da102ad050b75968829653fca0c763b6
Avani <avani.jadeja@gmail.com>
2023-06-10 10:03:35 -0500

    Created index.js file for api routes

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+// require express router
+const router = require('express').Router();
+// require user-routes
+const userRoutes = require('./user-routes');
+// require thought-routes
+const thoughtRoutes = require('./thought-routes');
+
+//  set route path 
+router.user('/users',userRoutes);
+router.use('./thoughts',thoughtRoutes);
+
+// export router
+module.exports=router;
