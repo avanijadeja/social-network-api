95f484cbae3300363b1c38ef47038fe0a130ef6e
Avani <avani.jadeja@gmail.com>
2023-06-10 18:47:35 -0500

    Added logic for get single thought in thought-router.js file

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,15 @@
 // require express router
 const router = require("express").Router();
 
-const { createThought,getThoughts } = require("../../controllers/thought-controller");
+const {
+  createThought,
+  getThoughts,
+} = require("../../controllers/thought-controller");
 
 // set route /api/thoughts for function createThought
 router.route("/").post(createThought).get(getThoughts);
 
+// set route /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought);
 // export router
 module.exports = router;
