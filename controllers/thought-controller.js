3cbd68c7eddbdb9e9bb11fab92641164703d0a23
Avani <avani.jadeja@gmail.com>
2023-06-10 20:00:40 -0500

    change update logic

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -75,7 +75,37 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  
+
+  // Delete a single Thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res
+            .status(404)
+            .json({ message: "Thought with this Id does not exit." });
+        }
+
+        // Remove Thought id from user's thoughts field
+        return User.findOneAndUpdate(
+          { thoughts: req.params.thoughtId },
+          { $pull: { thoughts: req.params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({
+            message: "Thought created but no user found with this Id",
+          });
+        }
+        res.json({ message: "Deleted Thought!" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 // export thoghtController
